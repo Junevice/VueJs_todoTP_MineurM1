d929b239d5324213f0c54971e849514d41f80722
Junevice <june.webfreelance@gmail.com>
2023-04-14 15:10:02 +0200

    Make tailwind work

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,6 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1 class="text-3xl underline">hello world</h1>
   </main>
 </template>
